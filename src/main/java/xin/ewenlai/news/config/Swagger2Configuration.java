df57d2dc67e1041e5c3c687a60a031f7f94e1152
Lwwen <lwwen1107@qq.com>
2018-07-11 16:03:07 +0800

    添加 Swagger 配置类

---
+++ src/main/java/xin/ewenlai/news/config/Swagger2Configuration.java
@@ -0,0 +1,33 @@
+package xin.ewenlai.news.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Configuration {
+    @Bean
+    public Docket createDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(createApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("xin.ewenlai.news"))
+                .paths()
+                .build();
+    }
+
+    private ApiInfo createApiInfo() {
+        return new ApiInfoBuilder()
+                .title("新闻浏览 APP 的 RESTful 风格 APIs")
+                .description("https://www.github.com/52101107")
+                .termsOfServiceUrl("https://www.github.com/52101107")
+                .version("0.0.1-SNAPSHOT")
+                .build();
+    }
+}
