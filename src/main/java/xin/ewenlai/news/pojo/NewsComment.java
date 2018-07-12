85ff6f29bc2c804494e8e7492b83facaac367171
Lwwen <lwwen1107@qq.com>
2018-07-12 17:02:25 +0800

    添加 NewsComment 类

---
+++ src/main/java/xin/ewenlai/news/pojo/NewsComment.java
@@ -0,0 +1,76 @@
+package xin.ewenlai.news.pojo;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.sql.Timestamp;
+
+/**
+ * description :
+ *
+ * @author lwwen
+ * date : 2018-07-12 16:17
+ * @version 0.0.0
+ */
+@Entity
+public class NewsComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long ID;
+
+    @NotNull
+    @ManyToOne
+    private User user;
+
+    @NotNull
+    private Timestamp time;
+
+    @NotNull
+    @Size(max = 200)
+    @Column(name = "newsURL")
+    private String newsURL;
+
+    @NotNull
+    @Size(max = 200)
+    private String content;
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public void setTime(Timestamp time) {
+        this.time = time;
+    }
+
+    public String getNewsURL() {
+        return newsURL;
+    }
+
+    public void setNewsURL(String newsURL) {
+        this.newsURL = newsURL;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
