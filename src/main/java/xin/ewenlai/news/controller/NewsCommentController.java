696e00960ecd1238a3d5f638660e869006f07254
Lwwen <lwwen1107@qq.com>
2018-07-16 08:43:21 +0800

    更改获取某新闻所有评论的API

--- src/main/java/xin/ewenlai/news/controller/NewsCommentController.java
+++ src/main/java/xin/ewenlai/news/controller/NewsCommentController.java
@@ -2,10 +2,7 @@ package xin.ewenlai.news.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import xin.ewenlai.news.pojo.NewsComment;
 import xin.ewenlai.news.service.NewsCommentService;
 import xin.ewenlai.news.utils.Code;
@@ -50,7 +47,6 @@ public class NewsCommentController {
             jsonObject.put("message", request.getParameter("username") + "评论失败。");
             NewsLogger.info(request.getParameter("username") + "评论失败。");
         }
-        jsonObject.put("data", null);
         return jsonObject;
     }
 
@@ -60,20 +56,20 @@ public class NewsCommentController {
      * @param newsURL 新闻链接
      * @return 返回 JSON 数据
      */
-    @RequestMapping(value = "/{newsURL}", method = RequestMethod.GET)
-    public JSONObject getCommentsByNewsURL(@PathVariable String newsURL) {
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public JSONObject getCommentsByNewsURL(@RequestParam String newsURL) {
         JSONObject jsonObject = new JSONObject();
         List<NewsComment> newsComments = newsCommentService.getComments(newsURL);
         if (newsComments != null) {
             jsonObject.put("code", Code.SUCCESS.getValue());
-            jsonObject.put("message", "获取评论成功。");
+            jsonObject.put("message", "获取" + newsURL + "评论成功。");
             jsonObject.put("data", newsComments);
-            NewsLogger.info("获取评论成功。");
+            NewsLogger.info("获取" + newsURL + "评论成功。");
         } else {
             jsonObject.put("code", Code.FAIL.getValue());
-            jsonObject.put("message", "获取评论失败。");
+            jsonObject.put("message", "获取" + newsURL + "评论失败。");
             jsonObject.put("data", null);
-            NewsLogger.info("获取评论失败。");
+            NewsLogger.info("获取" + newsURL + "评论失败。");
         }
         return jsonObject;
     }
